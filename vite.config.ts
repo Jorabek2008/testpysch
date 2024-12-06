0290d4159fd0c64eeb5eec80ea120468c383bd24
Jo'rabek <jorabekashurqulov400@gmail.com>
2024-12-06 13:56:38 +0500

    fixxxxxx

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["prop-types"],
-    },
-  },
   plugins: [
     react({
       jsxRuntime: "automatic", // Bu sozlamani tekshiring
